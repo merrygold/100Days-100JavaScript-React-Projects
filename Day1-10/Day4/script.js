25d2325a58895a305ee499da2fe313778745d61f
Wick.py <wickprof@gmail.com>
2023-01-04 23:31:38 +0500

    Popup Project

---
+++ Day1-10/Day4/script.js
@@ -0,0 +1,26 @@
+// First let's create some variables
+
+const modal = document.querySelector(".modal");
+
+const close = document.querySelector(".close");
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", openModal);
+
+// here when we click on close icon it will call the closeModal function to close the popup.
+close.addEventListener("click", closeModal);
+
+// here we are calling the closeModal function again so that the modal popup can be closed also when we click outside the popup box.
+modal.addEventListener("click", closeModal);
+
+
+function openModal(e) {
+    // we need to use event(e) parameter to prevent the page from refreshing.
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
